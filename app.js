771f3b8122badbf03aa922016c68752d9dbed821
DEEKSHA CHAUDHARY <deekshachaudhary2019@gmail.com>
2023-03-08 21:48:22 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+//var serverURL = "http://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-// outputDiv.innerText = "Deeksha Chaudhary"
-console.log(outputDiv);
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+return serverURL + "?" + "text=" + input }
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server try again after some time") }
 
 function clickHandler() {
-    outputDiv.innerText = "ajsjsjsjsjsjsj " + txtInput.value;
-};
+   var inputText = txtInput.value; // taking input 
 
+   //calling server for processing 
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText; }) // output
+    .catch(errorHandler)
+};
 btnTranslate.addEventListener("click",clickHandler)
 
